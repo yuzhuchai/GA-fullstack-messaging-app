409933a69d1710fdf718d656d807180e514ec86f
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-07-26 15:17:54 -0500

    setup server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
+const session = require("express-session")
+require("./db/db")
+
+app.use(session({
+	secret: "THIS IS A RANDOM SECRET STRING",
+	resave: false,
+	saveUninitialized: false
+}))
+
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(methodOverride("_method"))
+
+
+
+app.listen(PORT,() => {
+	console.log(`server listening at ${PORT}, ${new Date}`);
+})

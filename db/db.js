7c4432f5d33b2da052a7f82601003178006a0dc1
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-07-26 15:19:58 -0500

    setup database

---
+++ db/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://localhost/gallery';
+
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`mongoose connected to ${connectionString}`);
+});
+
+
+mongoose.connection.on('disconnected', () => {
+  console.log(`mongoose disconnected to ${connectionString}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`mongoose error to ${err}`);
+});

d039859b08b4e4fda3447ad9cfff8ecefb9f3019
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-07-27 15:46:38 -0500

    seems to be able to upload photo

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -6,13 +6,35 @@ const fs = require("fs")
 
 const upload = multer({dest: "uploads"})
 
-
+//photo index route, 
 router.get("/",(req,res,next)=>{
 	res.render("photo/index.ejs")
 })
 
+
+//photo new route 
 router.get("/new", (req,res,next)=>{
 	res.render("photo/create.ejs")
+})
+
+
+//photo post route 
+router.post("/", upload.single('photo'), async (req,res,next)=>{
+	// console.log(req.file);
+	// res.send(req.file)
+	//creates a file inside the uplaods folder.
+	const filePath = req.file.path
+	// console.log(req.body);
+	const createdPhoto = new Photo
+	createdPhoto.title = req.body.title
+	createdPhoto.date = req.body.date
+	createdPhoto.photo.data = fs.readFileSync(filePath)
+	createdPhoto.photo.contentType = req.file.mimetype
+
+	await createdPhoto.save()
+	console.log(createdPhoto, "<-------this is uploaded photo");
+
+
 })
 
 module.exports = router

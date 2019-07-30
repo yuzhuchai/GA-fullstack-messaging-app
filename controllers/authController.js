ab2ec442ddd3b343fb5dfb52626d922c07da5863
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-07-30 14:01:49 -0500

    redirect user and display message when enter incorrect username or password or try to register with a name already in use

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,9 +17,11 @@ router.post('/login', async (req, res, next) => {
         res.redirect('/photos/' + req.session.userId)
       } else {
         req.session.message = 'Incorrect password';
+        res.redirect("/")
       }
     } else {
       req.session.message = 'Username not found';
+      res.redirect("/")
     }
   } catch (error) {
     next(err);
@@ -28,22 +30,22 @@ router.post('/login', async (req, res, next) => {
 
 
 router.post('/register', async (req, res) => {
-
-  const password = req.body.password;
-
-  // Encrypt inputed password
-  const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-
-  req.body.password = hashedPassword;
-
   try {
+      const foundUser = await User.find({username: req.body.username})
+      if (foundUser.length === 0){
+      const password = req.body.password;
+      // Encrypt inputed password
+      const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+      req.body.password = hashedPassword;
       const createdUser = await User.create(req.body);
-
       req.session.userId = createdUser._id;
       req.session.username = createdUser.username;
       req.session.loggedIn = true;
-
-      res.redirect('/photos/' + req.session.userId);
+      res.redirect('/photos/' + req.session.userId); 
+      } else {
+        req.session.message = "username already registered"
+        res.redirect("/user/new")
+      }
   } catch (err){
     res.send(err)
   }

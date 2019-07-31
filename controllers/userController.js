a5091cba0bb245ec1cdf7be53e6f8bf04a2c5127
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-07-31 00:34:33 -0500

    able to delete user, and the images user posted

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,25 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/user.js")
+const Photo = require("../models/photo.js")
+router.get('/:id/edit', (req, res, next) => {
+  res.send("hitting the edit route")
+})
 
-router.get('/user/edit', (req, res, next) => {
-  res.redirect('/photos')
+
+router.delete("/:id", async (req,res,next)=>{
+	try{
+		const foundUser = await User.findByIdAndRemove(req.params.id)
+		console.log(foundUser,"<-----found user in the delete route");
+		const foundPhotos = await Photo.deleteMany({user:req.params.id})
+		console.log(foundPhotos,"<----found photo in the delete route");
+		// need to find all the messages attached to the user too
+		// const foundMessages = await Message.find({photoId: foundPhotos._id})
+		// console.log(res);
+		res.redirect("/")
+	}catch(err){
+		next(err)
+	}
 })
 
 
--- models/message.js
+++ models/message.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
 const messageSchema = new mongoose.Schema({
-	photoId: [{
+	photoId: {
     type: mongoose.Schema.Types.ObjectId,  // ._id
     ref: 'Photo'
-  }],
+  },
 	date: {type: Date, default: Date.now},
 	title: String,
 	content: String,
